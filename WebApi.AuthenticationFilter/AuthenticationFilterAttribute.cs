5e618832becfd36646cf1964cc070666304dc015
Michael Benford <michael@michaelbenford.net>
2014-05-11 22:29:47 -0300

    Add async support
    Add OnAuthenticationAsync and OnAuthenticationChallengeAsync methods so
    async operations are supported.

--- WebApi.AuthenticationFilter/AuthenticationFilterAttribute.cs
+++ WebApi.AuthenticationFilter/AuthenticationFilterAttribute.cs
@@ -11,22 +11,12 @@ namespace WebApi.AuthenticationFilter
     public abstract class AuthenticationFilterAttribute : FilterAttribute, IAuthenticationFilter
     {        
         /// <summary>
-        /// Authenticates the request
+        /// Authenticates the request asynchronously.
         /// </summary>
         /// <param name="context">The authentication context.</param>
-        public virtual void OnAuthentication(HttpAuthenticationContext context)
-        {            
-        }
-
-        /// <summary>
-        /// Adds an authentication challenge to the inner IHttpActionResult.
-        /// </summary>
-        /// <param name="context">The authentication challenge context.</param>
-        public virtual void OnAuthenticationChallenge(HttpAuthenticationChallengeContext context)
-        {            
-        }
-
-        Task IAuthenticationFilter.AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A Task that will perform the authentication challenge.</returns>
+        public virtual Task OnAuthenticationAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
         {
             try
             {
@@ -40,13 +30,27 @@ namespace WebApi.AuthenticationFilter
             return Completed();
         }
 
-        Task IAuthenticationFilter.ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        /// <summary>
+        /// Authenticates the request.
+        /// </summary>
+        /// <param name="context">The authentication context.</param>
+        public virtual void OnAuthentication(HttpAuthenticationContext context)
+        {
+        }
+
+        /// <summary>
+        /// Adds an authentication challenge to the inner IHttpActionResult asynchronously.
+        /// </summary>
+        /// <param name="context">The authentication challenge context.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A Task that will perform the authentication challenge.</returns>
+        public virtual Task OnAuthenticationChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
         {
             try
             {
-                OnAuthenticationChallenge(context);                
+                OnAuthenticationChallenge(context);
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return FromError(ex);
             }
@@ -54,11 +58,39 @@ namespace WebApi.AuthenticationFilter
             return Completed();
         }
 
+        /// <summary>
+        /// Adds an authentication challenge to the inner IHttpActionResult.
+        /// </summary>
+        /// <param name="context">The authentication challenge context.</param>
+        public virtual void OnAuthenticationChallenge(HttpAuthenticationChallengeContext context)
+        {
+        }
+
+        Task IAuthenticationFilter.AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            return AuthenticateAsync(context, cancellationToken);
+        }
+
+        Task IAuthenticationFilter.ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            return ChallengeAsync(context, cancellationToken);
+        }
+
+        private async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            await OnAuthenticationAsync(context, cancellationToken);
+        }
+
+        private async Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            await OnAuthenticationChallengeAsync(context, cancellationToken);
+        }
+
         private static Task Completed()
         {
             return Task.FromResult(default(AsyncVoid));
         }
-        
+
         private static Task<AsyncVoid> FromError(Exception exception)
         {
             var tcs = new TaskCompletionSource<AsyncVoid>();
@@ -67,7 +99,7 @@ namespace WebApi.AuthenticationFilter
         }
 
         private struct AsyncVoid
-        {            
+        {
         }
-    }        
+    }
 }
